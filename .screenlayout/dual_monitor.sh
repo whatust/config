8fd90cccbd40ca12e1783d9b70b1b6e8d3c8798a
whatust <whatust@gmail.com>
2020-11-14 01:15:52 -0300

    Fix xrandr startup problem

--- .screenlayout/dual_monitor.sh
+++ .screenlayout/dual_monitor.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output DVI-I-1 --mode 1920x1080 --pos 1920x0 --rotate left --output DP-1 --off --output DP-2 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x731 --rotate normal --output DP-3 --off
+sleep 1
+xrandr --output DVI-I-1 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-1 --primary --mode 1920x1080 --pos 0x731 --rotate normal
+xwallpaper --output DVI-I-1 --zoom ~/Images/wallpapers/vertical/dark_souls-0001.png --output HDMI-1 --zoom ~/Images/wallpapers/horizontal/dark_souls-0000.png
